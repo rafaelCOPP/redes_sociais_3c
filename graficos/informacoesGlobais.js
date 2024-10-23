33cf2d9610ef4e9e2656bf9b295845ccbe00d9e1
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-10-23 07:45:48 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,6 +1,9 @@
-var cont
+const url = "";
 
-while(cont<10){
-    document.write("Vai toma no cu arom")
-    i++
+async function visualizarInformacoesGlobais(){
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
 }
+
+visualizarInformacoesGlobais();
